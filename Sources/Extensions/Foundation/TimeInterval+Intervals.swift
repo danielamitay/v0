464d9cfff663ea7ef8b88dcf99bae88c06e260f0
danielamitay <hello@danielamitay.com>
2024-10-01 23:05:08 -0400

    Update TimeInterval+Intervals.swift

--- Sources/Extensions/Foundation/TimeInterval+Intervals.swift
+++ Sources/Extensions/Foundation/TimeInterval+Intervals.swift
@@ -8,12 +8,46 @@
 import Foundation
 
 public extension TimeInterval {
+    /// A minute in seconds.
     static let minute: TimeInterval = 60
+    /// An hour in seconds.
     static let hour: TimeInterval = 60 * minute
+    /// A day in seconds.
     static let day: TimeInterval = 24 * hour
+    /// A week in seconds.
     static let week: TimeInterval = 7 * day
+}
 
-    // Worth noting that months and years are not constant intervals!
-    static let month: TimeInterval = 30 * day
-    static let year: TimeInterval = 365 * day
+public extension TimeInterval {
+    /// Convert the value to milliseconds `TimeInterval`.
+    var millisecond: TimeInterval { self / 1000 }
+    /// Convert the value to milliseconds `TimeInterval`.
+    var milliseconds: TimeInterval { self / 1000 }
+    /// Convert the value to milliseconds `TimeInterval`.
+    var ms: TimeInterval { self / 1000 }
+
+    /// Convert the value to seconds `TimeInterval`.
+    var second: TimeInterval { self }
+    /// Convert the value to seconds `TimeInterval`.
+    var seconds: TimeInterval { self }
+
+    /// Convert the value to minutes `TimeInterval`.
+    var minute: TimeInterval { self * 60 }
+    /// Convert the value to minutes `TimeInterval`.
+    var minutes: TimeInterval { self * 60 }
+
+    /// Convert the value to hours `TimeInterval`.
+    var hour: TimeInterval { self * 3600 }
+    /// Convert the value to hours `TimeInterval`.
+    var hours: TimeInterval { self * 3600 }
+
+    /// Convert the value to days `TimeInterval`.
+    var day: TimeInterval { self * 3600 * 24 }
+    /// Convert the value to days `TimeInterval`.
+    var days: TimeInterval { self * 3600 * 24 }
+
+    /// Convert the value to weeks `TimeInterval`.
+    var week: TimeInterval { self * 3600 * 24 * 7 }
+    /// Convert the value to weeks `TimeInterval`.
+    var weeks: TimeInterval { self * 3600 * 24 * 7 }
 }
