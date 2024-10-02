5cd8d65c49d5509dcf4c140732912d7490ea1e9d
danielamitay <hello@danielamitay.com>
2024-10-01 22:50:25 -0400

    Update Clamped.swift

--- Sources/Math/Clamped.swift
+++ Sources/Math/Clamped.swift
@@ -6,7 +6,13 @@
 //
 
 public extension Comparable {
+    /// Clamps the value to a given range.
     func clamped(to range: ClosedRange<Self>) -> Self {
-        return min(max(self, range.lowerBound), range.upperBound)
+        return clamp(self, range.lowerBound, range.upperBound)
     }
+}
+
+/// Clamps a value to a given range.
+@inlinable public func clamp<T>(_ value: T, _ min: T, _ max: T) -> T where T : Comparable {
+    return Swift.min(Swift.max(value, min), max)
 }
